26340b309eb037ae85723b0575267e3375455437
FacundoM-Utn <facundo_mori@outlook.com>
2026-09-04 22:00:38 -0300

    del 1 al 4

--- practica/Facundo-Mori/consultas-tabla.sql
+++ practica/Facundo-Mori/consultas-tabla.sql
@@ -1,1 +1,18 @@
---Consultas en progreso
+--ejrcici1 
+select *
+from dueno
+order by apellido;
+
+-- Ej2
+select nombre, especie
+from mascota;
+
+-- 3ejercicio
+select *
+from mascota
+where peso_kg > 10;
+
+-- ejercicio4
+select *
+from veterinario
+where especialidad = 'Clínica general';
